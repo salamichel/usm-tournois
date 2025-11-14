93da7c23769888c5ae6847e1bb77e3bb64f388ea
Stéphane Alamichel <3256040+salamichel@users.noreply.github.com>
2025-11-14 17:40:34 +0100

    Rename vite.config.js to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // or vue(), etc.
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], // Your other plugins
-  
-  // Add this 'server' section
+  // ... potentiellement d'autres plugins comme react() ou vue()  
   server: {
     allowedHosts: [
       'usm-tournois.moka-web.net'
